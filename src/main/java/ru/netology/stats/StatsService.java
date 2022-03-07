d27d9a0bcee4f31e0c5f45fc1196c316dfcd18b3
Olga Fedotova <bursh1-ola@yandex.ru>
2022-03-07 21:07:41 +0700

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,73 @@
+package ru.netology.stats;
+
+public class StatsService {
+    // Сумма всех продаж
+    public long calculateSum (long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    //Средняя сумма продаж в месяц
+
+    public long calculateAvg(long[] sales) {
+        return calculateSum(sales) / sales.length;
+    }
+
+    //Номер месяца, в котором был пик продаж (осуществлены продажи на максимальную сумму)
+
+    public long namberMaxMonthSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month += 1;
+        }
+        return maxMonth + 1;
+    }
+
+    //Номер месяца, в котором был минимум продаж (осуществлены продажи на минимальную сумму)
+
+    public long namberMinMonthSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month += 1;
+        }
+        return minMonth + 1;
+    }
+    //Кол-во месяцев, в которых продажи были ниже среднего
+
+    public long countMonthSalesLessAvg (long[] sales) {
+        int month = 0;
+        for (long sale : sales) {
+            if (sale < calculateAvg(sales)) {
+                month += 1;
+
+            }
+
+        }
+        return month;
+    }
+    //Кол-во месяцев, в которых продажи были выше среднего
+
+    public long countMonthSalesOverAvg (long[] sales) {
+        int month = 0;
+        for (long sale : sales) {
+            if (sale > calculateAvg(sales)) {
+                month += 1;
+
+            }
+
+        }
+        return month;
+    }
+
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,75 @@
+package ru.netology.stats;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldcalculateSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+
+        long actual = service.calculateSum(sales);
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldcalculateAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+
+        long actual = service.calculateAvg(sales);
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldcalculateNamberMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+
+        long actual = service.namberMaxMonthSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldcalculateNamberMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+
+        long actual = service.namberMinMonthSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldcalculateCountMonthSalesLessAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+
+        long actual = service.countMonthSalesLessAvg(sales);
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldcalculateCountMonthSalesOverAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+
+        long actual = service.countMonthSalesOverAvg(sales);
+
+        assertEquals(expected, actual);
+
+    }
+}
